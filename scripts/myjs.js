e0e814e4ba22996bbb2e19250b4afebe4525eddc
Hessam <hesamnik@gmail.com>
2017-01-16 22:39:15 +0100

    Adds website files
    First version

---
+++ scripts/myjs.js
@@ -0,0 +1,26 @@
+﻿function collapseNavBar() {
+    nv = document.getElementById('bs-navbar-collapse');
+    nv.classList.remove('in');
+    nv.setAttribute('aria-expanded', 'false');
+}
+
+function replaceText(idname) {
+    mt = document.getElementById('maintext');
+    txtarget = document.getElementById(idname);    
+    mt.innerHTML = txtarget.innerHTML;    
+    navEL = document.getElementById('navEl');
+    for (kk = 0; kk < navEL.childElementCount; kk++) { navEL.children[kk].className = ""; }
+    targetnav = document.getElementById('nav' + idname);
+    targetnav.className = "active";
+
+    collapseNavBar();
+}
+
+document.addEventListener("DOMContentLoaded", function () { replaceText('HOME') });
+document.getElementById('navHOME').addEventListener("click", function () { replaceText('HOME') });
+document.getElementById('navFEATURES').addEventListener("click", function () { replaceText('FEATURES') });
+document.getElementById('navTECHNOLOGY').addEventListener("click", function () { replaceText('TECHNOLOGY') });
+document.getElementById('navAPPLICATION').addEventListener("click", function () { replaceText('APPLICATION') });
+document.getElementById('navTEAM').addEventListener("click", function () { replaceText('TEAM') });
+document.getElementById('navCONTACT').addEventListener("click", function () { replaceText('CONTACT') });
+document.getElementById('navNEWS').addEventListener("click", function () { replaceText('NEWS') });
